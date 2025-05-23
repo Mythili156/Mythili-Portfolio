b6636e3ee1d73a6eeaffc8bc14a93ed791602f08
Mythili156 <153004073+Mythili156@users.noreply.github.com>
2025-05-23 11:05:46 +0530

    Update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -48,7 +48,7 @@ const projects = [
   {
     title: "Credit Card Fraud Detection using Deep Learning",
     description: "Created a deep learning model to detect fraudulent credit card transactions in real-time.",
-    image: "/public/credit.png",
+    image: "https://www.xenonstack.com/hubfs/xenonstack-credit-card-fraud-detection.png",
     link: "#",
     github: "#",
   },
